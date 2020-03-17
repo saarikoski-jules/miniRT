0af574b26bea565c03ea448e98da7bc19a1c593b
jvsr <jellevisser00@gmail.com>
2020-03-17 14:53:03 +0100

    Update error.c

--- error.c
+++ error.c
@@ -6,11 +6,11 @@
 /*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/15 18:29:28 by jvisser        #+#    #+#                */
-/*   Updated: 2020/03/15 18:44:35 by jvisser       ########   odam.nl         */
+/*   Updated: 2020/03/17 14:49:49 by jvisser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdlib.h>
 
 #include "libft.h"
 
