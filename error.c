ef48e0e997bce653f74ba9b3a144fff66ebf5aff
jvsr <jellevisser00@gmail.com>
2020-03-18 16:05:07 +0100

    Adding errno error mangement

--- error.c
+++ error.c
@@ -6,13 +6,22 @@
 /*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/15 18:29:28 by jvisser        #+#    #+#                */
-/*   Updated: 2020/03/17 14:49:49 by jvisser       ########   odam.nl         */
+/*   Updated: 2020/03/18 14:09:25 by jvisser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "libft.h"
+
+void	error_exit_errno(void)
+{
+	ft_printf("Error\n");
+	ft_printf("MiniRT(%d): %s\n", errno, strerror(errno));
+	exit(errno);
+}
 
 void	error_exit_msg(const unsigned int e_code, const char *e_msg)
 {
