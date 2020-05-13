c9fd0dc268ad1ef95a0d2966856e9d832e4322f2
saarikoski-jules <jules.saarikoski@gmail.com>
2020-05-13 18:54:02 +0200

    refactored tr and sq collision detection

--- rendering/collision.c
+++ rendering/collision.c
@@ -42,36 +42,34 @@ double pl_intersect(t_vec *orien, t_vec *ray_start, t_vec *pos, t_vec *ray)
 	return (t);
 }
 
+double point_within_line(t_vec *point1, t_vec *point2, t_vec *p, t_vec *orien)
+{
+	t_vec *edge;
+	t_vec *to_p;
+	t_vec *cross;
+	double dot;
+
+	edge = substract_vectors(point2, point1);
+	to_p = substract_vectors(p, point1);
+	cross = get_cross_product(edge, to_p);
+	dot = get_dot_product(orien, cross);
+	return (dot);
+}
+
 double sq_intersect(t_vec *ray_start, t_vec *ray, t_sq *sq)
 {
 	double t;
+	t_vec *point;
 
 	t = pl_intersect(sq->orien, ray_start, sq->point1, ray);
 	if (t == INFINITY)
 		return (INFINITY);
-	t_vec *point = gen_coord(t * ray->x, t * ray->y, t * ray->z); //account for different camera pos
+	point = find_point(ray_start, ray, t);
 
-	t_vec *intersect = add_vectors(ray_start, point);
-
-	t_vec *edge1 = substract_vectors(sq->point2, sq->point1);
-	t_vec *edge2 = substract_vectors(sq->point3, sq->point2); 
-	t_vec *edge3 = substract_vectors(sq->point4, sq->point3); 
-	t_vec *edge4 = substract_vectors(sq->point1, sq->point4);
-
-	t_vec *P1 = substract_vectors(intersect, sq->point1);
-	t_vec *P2 = substract_vectors(intersect, sq->point2);
-	t_vec *P3 = substract_vectors(intersect, sq->point3);
-	t_vec *P4 = substract_vectors(intersect, sq->point4);
-
-	t_vec *cross1 = get_cross_product(edge1, P1);
-	t_vec *cross2 = get_cross_product(edge2, P2);
-	t_vec *cross3 = get_cross_product(edge3, P3);
-	t_vec *cross4 = get_cross_product(edge4, P4);
-
-	if (get_dot_product(sq->orien, cross1) > 0
-		&& get_dot_product(sq->orien, cross2) > 0
-		&& get_dot_product(sq->orien, cross3) > 0
-		&& get_dot_product(sq->orien, cross4) > 0)
+	if (point_within_line(sq->point1, sq->point2, point, sq->orien) > 0
+	&&	point_within_line(sq->point2, sq->point3, point, sq->orien) > 0
+	&&	point_within_line(sq->point3, sq->point4, point, sq->orien) > 0
+	&&	point_within_line(sq->point4, sq->point1, point, sq->orien) > 0)
 	{
 		return (t);
 	}
@@ -94,17 +92,17 @@ double get_cy_endcap(t_vec *pos, t_vec *ray_start, t_vec *ray, t_cy *cy)
 	t_vec	*point;
 
 	t = pl_intersect(cy->orien, ray_start, pos, ray);
-	if (t == INFINITY)
-		return (INFINITY);
+	if (t == NO_INTERSECT)
+		return (NO_INTERSECT);
 	point = find_point(ray_start, ray, t);
 	d = get_distance(point, pos);
 	free(point);
 	if (d < cy->r)
 		return (t);
-	return (INFINITY);
+	return (NO_INTERSECT);
 }
 
-double get_shaft_intersection_eight(t_vec *ray_start, t_vec *ray, t_cy *cy)
+double get_shaft_intersection(t_vec *ray_start, t_vec *ray, t_cy *cy)
 {
 
 	t_vec *O; //check if vector starts inside the cylinder
@@ -151,48 +149,57 @@ double cy_intersect(t_vec *ray_start, t_vec *ray, t_cy *cy)
 	double t2;
 	double t3;
 
+	t3 = get_shaft_intersection(ray_start, ray, cy);
+	if (t3 == INSIDE_OBJ)
+		return(INSIDE_OBJ);
 	t1 = get_cy_endcap(cy->end1, ray_start, ray, cy);
 	t2 = get_cy_endcap(cy->end2, ray_start, ray, cy);
-	t3 = get_shaft_intersection_eight(ray_start, ray, cy);
-    if (t3 == -10)
-        return(-10);
-	t = t1;
-	if (t2 < t)
-	{
+	t = INFINITY;
+	if (t1 != NO_INTERSECT)
+		t = t1;
+	if (t2 != NO_INTERSECT && t2 < t)
 		t = t2;
-	}
-	if (t3 < t)
-	{
+	if ( t3 != NO_INTERSECT && t3 < t)
 		t = t3;
-	}
 	return (t);
 }
 
 double tr_intersect(t_vec *ray_start, t_vec *ray, t_tr *tr)
 {
-	double t = pl_intersect(tr->orien, ray_start, tr->point1, ray);
-	if (t == INFINITY)
-		return (INFINITY);
-	t_vec *point = gen_coord(t * ray->x, t * ray->y, t * ray->z); //account for different camera position
-	t_vec *intersect = add_vectors(ray_start, point);
-	t_vec *edge1 = substract_vectors(tr->point2, tr->point1); 
-	t_vec *edge2 = substract_vectors(tr->point3, tr->point2); 
-	t_vec *edge3 = substract_vectors(tr->point1, tr->point3);
+	double t;
+	t_vec *point;
+	t = pl_intersect(tr->orien, ray_start, tr->point1, ray);
+	if (t == NO_INTERSECT)
+		return (NO_INTERSECT);
+	point = find_point(ray_start, ray, t);
+	// t_vec *point = gen_coord(t * ray->x, t * ray->y, t * ray->z); //account for different camera position
+	// t_vec *intersect = add_vectors(ray_start, point);
+	// t_vec *edge1 = substract_vectors(tr->point2, tr->point1); 
+	// t_vec *edge2 = substract_vectors(tr->point3, tr->point2); 
+	// t_vec *edge3 = substract_vectors(tr->point1, tr->point3);
 
-	t_vec *P1 = substract_vectors(intersect, tr->point1);
-	t_vec *P2 = substract_vectors(intersect, tr->point2);
-	t_vec *P3 = substract_vectors(intersect, tr->point3);
+	// t_vec *P1 = substract_vectors(intersect, tr->point1);
+	// t_vec *P2 = substract_vectors(intersect, tr->point2);
+	// t_vec *P3 = substract_vectors(intersect, tr->point3);
 
-	t_vec *cross1 = get_cross_product(edge1, P1);
-	t_vec *cross2 = get_cross_product(edge2, P2);
-	t_vec *cross3 = get_cross_product(edge3, P3);
+	// t_vec *cross1 = get_cross_product(edge1, P1);
+	// t_vec *cross2 = get_cross_product(edge2, P2);
+	// t_vec *cross3 = get_cross_product(edge3, P3);
 
-	if (get_dot_product(tr->orien, cross1) > 0
-		&& get_dot_product(tr->orien, cross2) > 0
-		&& get_dot_product(tr->orien, cross3) > 0)
+
+	if (point_within_line(tr->point1, tr->point2, point, tr->orien) > 0
+	&&	point_within_line(tr->point2, tr->point3, point, tr->orien) > 0
+	&&	point_within_line(tr->point3, tr->point1, point, tr->orien) > 0)
 	{
 		return (t);
 	}
-	else
-		return (-1.0);
+
+	// if (get_dot_product(tr->orien, cross1) > 0
+		// && get_dot_product(tr->orien, cross2) > 0
+		// && get_dot_product(tr->orien, cross3) > 0)
+	// {
+		// return (t);
+	// }
+	// else
+	return (NO_INTERSECT);
 }
