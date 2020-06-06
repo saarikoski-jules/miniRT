a5a1e3e14df12f490c43be913b37f8f7713a5b22
saarikoski-jules <jsaariko@student.codam.nl>
2020-06-06 17:34:26 +0200

    Generating valid bmp header

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/03/14 14:09:53 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/06/02 10:26:02 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/06/06 17:33:57 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "error.h"
 #include "libft.h"
 #include "rt.h"
+#include <fcntl.h>//
 
 void print_scene(t_rt_scene *scene)
 {
@@ -115,13 +116,157 @@ void print_scene(t_rt_scene *scene)
 // 	img_ptr = mlx_new_image(mlx_ptr, 12, 12);
 // }
 
+// typedef struct{
+//     uint8_t signature[2];
+//     uint32_t filesize;
+//     uint32_t reserved;
+//     uint32_t fileoffset_to_pixelarray;
+// } fileheader;
+// typedef struct{
+//     uint32_t dibheadersize;
+//     uint32_t width;
+//     uint32_t height;
+//     uint16_t planes;
+//     uint16_t bitsperpixel;
+//     uint32_t compression;
+//     uint32_t imagesize;
+//     uint32_t ypixelpermeter;
+//     uint32_t xpixelpermeter;
+//     uint32_t numcolorspallette;
+//     uint32_t mostimpcolor;
+// } bitmapinfoheader;
+// typedef struct {
+//     fileheader fileheader;
+//     bitmapinfoheader bitmapinfoheader;
+// } bitmap;
+
+
+
+// typedef struct	s_bitmap_header
+// {
+	// t_file_header	file_header;
+	// t_info_header	info_header;
+// }				t_bitmap_header;
+
+
+// t_file_header gen_file_header()
+// {
+
+// 	return (file_header);
+// }
+
+// t_info_header gen_info_header()
+// {
+// 	return (info_header);
+// }
+
+// int	input_header(int fd, t_bitmap_header header)
+// {
+	// return (0);
+// }
+
+
+typedef struct	s_file_header
+{
+	uint8_t		file_type1; //BM (should i do two uint8 for B and M ?)
+	uint8_t		file_type2; //BM (should i do two uint8 for B and M ?)
+	uint32_t	file_size; //? 54 bytes header + pixels
+	uint16_t	reserved; //0
+	uint16_t	reserved2; //maybe you dont need this and they can be one uint32 with value 0
+	uint32_t	pixel_offset; // 54? AAA 14 bytes for file header + 40 bytes for info header
+}				t_file_header;
+
+typedef struct	s_info_header
+{
+	uint32_t	info_header_size; //40
+	uint32_t	pixel_x; //res_x
+	uint32_t	pixel_y; //res_y
+	uint16_t	planes; //1 ????
+	uint16_t	bits_per_pixel; //24 ??
+	uint32_t	compression; // 0 (no compression used)
+	uint32_t	img_size; //when no compression used can be set to 0 ????
+	uint32_t	pix_p_meter_x; //can be set to 0 ????
+	uint32_t	pix_p_meter_y; //can be set to 0 ????
+	uint32_t	colors; //can be set to 0
+	uint32_t	important_colors; //0 (all colors important)
+}				t_info_header;
+
+int	gen_bmp_header(int fd, size_t img_size)
+{
+	char f_header[14];
+	size_t file_size = 54 + img_size;
+
+	ft_bzero(f_header, 14);
+
+	f_header[0] = 'B'; //8 file type
+	f_header[1] = 'M'; //16
+	//might need a padding byte here??
+	f_header[2] = (uint32_t)file_size; //16 + 32 = 48
+	f_header[6] = (uint32_t)0; //reserved
+	f_header[10] = (uint32_t)54; //pixel offset
+
+	write(1, &f_header, 14);
+	write(fd, &f_header, 14);
+
+	char i_header[40];
+	ft_bzero(i_header, 40);
+
+	i_header[0] = (uint32_t)40; //header size
+	i_header[4] = (uint32_t)1; //res_x
+	i_header[8] = (uint32_t)1; //res_y
+	i_header[12] = (uint16_t)1; //planes
+	i_header[14] = (uint16_t)24; //bits per pixel
+	i_header[16] = (uint32_t)0; //compression
+	i_header[20] = (uint32_t)0; // img size, (can be set to 0 when no compression used)
+	i_header[24] = (uint32_t)0; //pix per meter x (can be set to zero)
+	i_header[28] = (uint32_t)0; //pix per meter y (can be set to zero)
+	i_header[32] = (uint32_t)0; //colors (can be set to zero?)
+	i_header[36] = (uint32_t)0; //important colors (0 means all colors are important)
+	write(1, "\nlol\n", 5);
+
+	write(1, &i_header, 40);
+	write(fd, &i_header, 40);
+
+	// fwrite(&f_header, 2, 1, fd);
+	return (1);
+}
+
+int append_pixels(int fd)
+{
+	char str[4];
+	ft_bzero(str, 4);
+	write(fd, &str, 4);
+	return(1);
+}
+
 int main(int ac, char **av)
 {
 	t_rt_scene	*scene;
-	if (ac == 2)
+	if (ac == 3 && ft_strncmp("--save", av[2], 7) == 0) //make sure there's a valid path
+	{
+
+		// ft_printf("%s\n", av[2]);
+		size_t len = ft_strlen(av[1]);
+		char *name = ft_substr(av[1], 0, len - 3);
+		char *name_bmp = ft_strjoin(name, ".bmp");
+		// char *name_bmp = ft_strjoin(name, ".bmp");
+		ft_printf("%s\n", name_bmp);
+		// write(1, "name", 4);
+		int fd = open(name_bmp, O_RDWR | O_CREAT | O_APPEND); //will not overwrite with new .bmp.
+
+		size_t img_size = 1;
+		
+		gen_bmp_header(fd, img_size);
+		append_pixels(fd);
+		// write(fd, name_bmp, len + 1);
+		return (0);
+	}
+	else if (ac == 2)
 		scene = get_scene(av[1]);
 	else
 		error_exit_msg(C_MAIN_FEW_ARGUMENTS, E_MAIN_FEW_ARGUMENTS);
+	// else if (ac == 3 && ft_strncmp("--save", av[2]))
+		// ft_printf("saving");
 	
 	// print_scene(scene);
 
