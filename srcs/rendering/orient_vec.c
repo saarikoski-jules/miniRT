625ad9ca9a1abf81046b829e013b2d3883960ddf
saarikoski-jules <jsaariko@student.codam.nl>
2020-06-23 10:20:13 +0200

    Changed to working orient_vec

--- srcs/rendering/orient_vec.c
+++ srcs/rendering/orient_vec.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/15 17:17:13 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/06/22 12:15:46 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/06/23 10:19:41 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,66 @@
 #include "error.h"
 #include <math.h>
 
-static t_vec	*parallel_vecs(t_qua *q, t_vec *v)
+/*
+**	v_new	= q*v*q_con
+**		= (w,qv)(0,v)(w,-qv) //scalar part results in 0
+**		= 2(qv⋅v)qv+(w^2−qv⋅qv)v+2w(qv×v)
+*/
+
+static t_vec	*orient_vec_util_two(t_qua *q, t_vec *v)
 {
-	if (q->w == 0.0)
-		return (gen_coord(-v->x, -v->y, -v->z));
-	return (gen_coord(v->x, v->y, v->z));
+	t_qua	*q_con;
+	double	mul;
+	t_vec	*vec;
+
+	q_con = gen_q_conjugate(q);
+	mul = pow(q->w, 2) + get_dot_product(q_con->vector, q->vector);
+	vec = gen_coord(mul * v->x, mul * v->y, mul * v->z);
+	free(q_con->vector);
+	free(q_con);
+	return (vec);
 }
 
-static t_qua	*gen_pure_quat(t_vec *v)
+static t_vec	*orient_vec_util(t_qua *q, t_vec *v)
 {
-	t_vec	*v_u;
-	t_qua	*q;
+	double	mul;
+	t_vec	*vec_one;
+	t_vec	*vec_two;
+	t_vec	*final;
 
-	v_u = set_vec_len(v, 1.0);
-	q = (t_qua *)e_malloc(sizeof(t_qua));
-	q->w = 0.0;
-	q->vector = gen_coord(v_u->x, v_u->y, v_u->z);
-	free(v_u);
-	return (q);
+	mul = 2 * get_dot_product(q->vector, v);
+	vec_one = gen_coord(q->vector->x * mul,
+						q->vector->y * mul,
+						q->vector->z * mul);
+	vec_two = orient_vec_util_two(q, v);
+	final = add_vectors(vec_one, vec_two);
+	free(vec_one);
+	free(vec_two);
+	return (final);
 }
 
 t_vec			*orient_vector(t_qua *q, t_vec *v)
 {
-	t_qua *q_v;
-	t_qua *tmp;
-	t_qua *q_c;
-	t_qua *final;
-	t_vec *vec_oriented;
+	double	mul;
+	t_vec	*cross;
+	t_vec	*vec_one;
+	t_vec	*vec_two;
+	t_vec	*vec_new;
 
-	if (det_len_vec(q->vector) == 0)
-		return (parallel_vecs(q, v));
-	q_v = gen_pure_quat(v);
-	tmp = multiply_quats(q, q_v);
-	q_c = gen_q_conjugate(q);
-	final = multiply_quats(tmp, q_c);
-	vec_oriented = gen_coord(final->vector->x,
-								final->vector->y,
-								final->vector->z);
-	free_quaternion(&q_v);
-	free_quaternion(&tmp);
-	free_quaternion(&q_c);
-	free_quaternion(&final);
-	return (vec_oriented);
+	if (q->w == 0.0
+		&& q->vector->x == 0.0
+		&& q->vector->y == 0.0
+		&& q->vector->z == 0.0)
+		return (gen_coord(-v->x, -v->y, -v->z));
+	vec_one = orient_vec_util(q, v);
+	mul = q->w * 2;
+	cross = get_cross_product(q->vector, v);
+	vec_two = gen_coord(cross->x * mul,
+						cross->y * mul,
+						cross->z * mul);
+	vec_new = add_vectors(vec_one, vec_two);
+	free(cross);
+	free(vec_one);
+	free(vec_two);
+	return (vec_new);
 }
